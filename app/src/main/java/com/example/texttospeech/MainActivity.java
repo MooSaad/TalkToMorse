f3e02ed1c78cfabccb89f98e8f9f38152b57ade3
moosaad <mahmoud.said.radwan@gmail.com>
2021-02-10 20:57:54 +0100

    TalkToMorse App
    A school project for IHM.

--- app/src/main/java/com/example/texttospeech/MainActivity.java
+++ app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -6,9 +6,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Vibrator;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -25,22 +28,119 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
     public static final Integer RecordAudioRequestCode = 1;
     private SpeechRecognizer speechRecognizer;
     private EditText editText;
     private ImageView micButton;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
-            checkPermission();
-        }
+
+        final TextView result;
+        final Button toMorseBtn;
 
         editText = findViewById(R.id.text);
         micButton = findViewById(R.id.button);
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        result = findViewById(R.id.result);
+        toMorseBtn = findViewById(R.id.toMorseBtn);
+
+        toMorseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txtToConvert = editText.getText().toString();
+                String convertedTxt = MorseCode.alphaToMorse(txtToConvert);
+                result.setText(convertedTxt);
+            }
+        });
+
+
+
+
+
+        Button b_Flash = findViewById(R.id.flash_Button);
+
+        b_Flash.setOnClickListener(
+                new Button.OnClickListener() {
+
+                    public void onClick(View v) {
+                        String s = editText.getText().toString().toLowerCase();
+                        if (MorseCode.vibrateTranslate(s)== "x") {
+                            result.setText(MorseCode.alphaToMorse(s));
+                        }
+                        else {
+                            MorseCode.flashInOwnThread(s);
+                        }
+                    }
+                }
+        );
+
+
+
+
+
+
+        Button b_Vibration = findViewById(R.id.vibrate_Button);
+
+        b_Vibration.setOnClickListener(
+                new Button.OnClickListener() {
+
+                    public void onClick(View v) {
+                        String s = editText.getText().toString().toLowerCase();
+                        if (MorseCode.vibrateTranslate(s)== "x") {
+                            result.setText(MorseCode.alphaToMorse(s));
+                        } else {
+                            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                            long dot = 500;          // Length of a Morse Code "dot" in milliseconds
+                            long dash = 1500;         // Length of a Morse Code "dash" in milliseconds
+                            long short_gap = 500;    // Length of Gap Between dots/dashes
+                            long medium_gap = 1500;   // Length of Gap Between Letters
+                            long long_gap = 3500;    // Length of Gap Between Words
+                            String temp = MorseCode.vibrateTranslate(s);
+                            ArrayList<Long> list = new ArrayList<Long>();
+                            list.add(new Long(0));
+                            for (char c : temp.toCharArray()) {
+                                if (c == '.') {
+                                    list.add(dot);
+                                } else if (c == '-') {
+                                    list.add(dash);
+                                } else if (c == ' ') {
+                                    list.add(short_gap);
+                                } else if (c == '!') {
+                                    list.add(medium_gap);
+                                } else if (c == '~') {
+                                    int index = list.lastIndexOf(medium_gap);
+                                    list.remove(index);
+                                    list.add(long_gap);
+                                }
+                            }
+                            long[] pattern = new long[list.size()];
+                            int i = 0;
+                            for (Long l : list) {
+                                long prim = l.longValue();
+                                pattern[i] = prim;
+                                i++;
+                            }
+                            vibrator.vibrate(pattern, -1);
+                        }
+                    }
+
+                }
+        );
+
+
+
+
+
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }
 
         final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -59,24 +159,13 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onRmsChanged(float v) {
-
-            }
-
+            public void onRmsChanged(float v) {}
             @Override
-            public void onBufferReceived(byte[] bytes) {
-
-            }
-
+            public void onBufferReceived(byte[] bytes) {}
             @Override
-            public void onEndOfSpeech() {
-
-            }
-
+            public void onEndOfSpeech() {}
             @Override
-            public void onError(int i) {
-
-            }
+            public void onError(int i) {}
 
             @Override
             public void onResults(Bundle bundle) {
@@ -86,14 +175,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onPartialResults(Bundle bundle) {
-
-            }
-
+            public void onPartialResults(Bundle bundle) {}
             @Override
-            public void onEvent(int i, Bundle bundle) {
-
-            }
+            public void onEvent(int i, Bundle bundle) {}
         });
 
         micButton.setOnTouchListener(new View.OnTouchListener() {
@@ -109,9 +193,8 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-
     }
+
 
     @Override
     protected void onDestroy() {
